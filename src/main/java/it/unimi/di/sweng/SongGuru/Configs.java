30d65cef4e1262a0d4c743973f4f1a1a3bf349c1
iLTeoooD <matteopietro.dazzi@gmail.com>
2017-06-25 21:55:57 +0200

    Fix auth problem

--- src/main/java/it/unimi/di/sweng/SongGuru/Configs.java
+++ src/main/java/it/unimi/di/sweng/SongGuru/Configs.java
@@ -13,6 +13,8 @@ public enum Configs {
 	public final String SERVER_TOKEN;
 	public final String BOT_TOKEN;
 	public final String ACCESS_TOKEN;
+	public final String CLIENT_ID;
+	public final String CLIENT_SECRET;
 
 	public final String END_POINT = "https://graph.facebook.com/v2.6/me/messages";
 	public final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -22,6 +24,8 @@ public enum Configs {
 		SERVER_TOKEN = System.getenv("TELEGRAM_SERVER_TOKEN");
 		BOT_TOKEN = System.getenv("TELEGRAM_BOT_TOKEN");
 		ACCESS_TOKEN = System.getenv("ACCESS_TOKEN");
+		CLIENT_ID = System.getenv("CLIENT_ID");
+		CLIENT_SECRET = System.getenv("CLIENT_SECRET");
 	}
 
 }
